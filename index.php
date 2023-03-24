05efafa8fe950914991128bdf543b648263dbe08
Ajit <ajitkp18@gmail.com>
2023-03-25 00:34:28 +0530

    added f project

--- index.php
+++
@@ -1,1 +0,0 @@
-<h1>Ajit Chutia hai </h1>
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:emart_app/views/splash_screen/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'consts/consts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //we are using getx so we have to chnage this matrial app into getmatrialapp
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: appname,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.transparent,
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.transparent),
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
